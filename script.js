9fdc5536f21e42d8148bb9731af0fd4ec5f28c7b
palafox12Ariana <palafox12@berkeley.edu>
2025-11-07 21:25:08 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicGFsYWZveDEyIiwiYSI6ImNtaDlybzE3ZjFma2UybG9hcXo2a3VqcXQifQ.dnUqdJp2dxXWYw2nJKqFvA';
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/palafox12/cmh9rt15h000101so8btkhmu0', // your Style URL goes here
+  style: 'mapbox://styles/palafox12/cmhp7he9v005f01re7z2m13x5', // mapbox://styles/palafox12/cmhp7he9v005f01re7z2m13x5
   center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9 // starting zoom
     });
